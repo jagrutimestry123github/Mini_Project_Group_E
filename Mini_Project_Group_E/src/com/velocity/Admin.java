4362c06d400fde5c437a0be6ca2e579d5dc24a45
Jagruti <jagrutimestry9075@gmail.com>
2024-07-01 15:50:51 +0530

    AdminImpl

--- Mini_Project_Group_E/src/com/velocity/Admin.java
+++ Mini_Project_Group_E/src/com/velocity/Admin.java
@@ -2,4 +2,24 @@ package com.velocity;
 
 public interface Admin {
 
+	// Method to get admin login details
+    public void getAdminLoginDetails();
+
+    // Method to add a product item
+    public void AddProductItem();
+
+    // Method to calculate bill
+    public void calculateBill();
+
+    // Method to display amount
+    public void displayAmount();
+
+    // Method to check quantity
+    public void checkQuantity();
+
+    // Method to check registered user
+    public void checkRegisteredUser();
+
+    // Method to check user history
+    public void checkUserHistory();
 }
---
+++ Mini_Project_Group_E/src/com/velocity/AdminImpl.java
@@ -0,0 +1,39 @@
+package com.velocity;
+
+public class AdminImpl implements Admin {
+
+	@Override
+    public void getAdminLoginDetails() {
+        // Implementation of login details retrieval
+    }
+
+    @Override
+    public void AddProductItem() {
+        // Implementation of adding a product item
+    }
+
+    @Override
+    public void calculateBill() {
+        // Implementation of bill calculation
+    }
+
+    @Override
+    public void displayAmount() {
+        // Implementation of amount display
+    }
+
+    @Override
+    public void checkQuantity() {
+        // Implementation of quantity checking
+    }
+
+    @Override
+    public void checkRegisteredUser() {
+        // Implementation of registered user checking
+    }
+
+    @Override
+    public void checkUserHistory() {
+        // Implementation of user history checking
+    }
+}
